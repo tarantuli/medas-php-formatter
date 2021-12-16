ef6fe6a1b39e84d7ccdec1f7d0baf68be12963b5
Patrick Morpey <infor@morphp.nl>
2021-12-16 16:52:27 +0100

    Line endings are required after some tokens, regardless of settings

--- src/BlockPrinter.php
+++ src/BlockPrinter.php
@@ -13,15 +13,22 @@ class BlockPrinter
 {
     private string $indentation;
     private string $lineEnding;
+    private int $lineEndingLength;
 
     public function print(Block $block, string $indentation, string $lineEnding): string
     {
         $this->indentation = $indentation;
         $this->lineEnding = $lineEnding;
+        $this->lineEndingLength = strlen($lineEnding);
 
         ob_start();
 
         $this->printBlock($block);
+
+        // A new line is required at the end
+        if ($this->lineEndingLength === 0) {
+            echo "\n";
+        }
 
         return ob_get_clean();
     }
@@ -50,6 +57,11 @@ class BlockPrinter
         }
 
         echo $this->lineEnding;
+
+        // A line ending is required after some tokens
+        if ($this->lineEndingLength === 0 && $statement->lastToken()->is([T_OPEN_TAG, T_COMMENT])) {
+            echo "\n";
+        }
 
         if ($statement->blankLineAfter) {
             echo $this->lineEnding;
