1de0e11d41b98178ee575336973e74c2070ae0b0
Patrick Morpey <patrick@morphp.nl>
2023-11-10 09:35:39 +0100

    Fixed bugs

--- tests/TestFiles/bugs2.php
+++ tests/TestFiles/bugs2.php
@@ -2,7 +2,10 @@
 
 declare(strict_types=1);
 
+use Medas\StorageManager\Interfaces\RecordSet;
+
 class ActionSet
 {
+    public RecordSet\Base|null $lastRecordSet = null;
     public mixed $lastInsertId = null;
 }
