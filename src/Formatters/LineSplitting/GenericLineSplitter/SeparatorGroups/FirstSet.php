941445899adf97bb5fe2f0b47753e38d334dc946
Patrick Morpey <patrick@morphp.nl>
2023-11-13 14:26:16 +0100

    Improved breakpoints

--- src/Formatters/LineSplitting/GenericLineSplitter/SeparatorGroups/FirstSet.php
+++ src/Formatters/LineSplitting/GenericLineSplitter/SeparatorGroups/FirstSet.php
@@ -19,6 +19,8 @@ class FirstSet implements SeparatorSet
         $this->groups = [
             new SeparatorGroup('ternary', [T_QUESTION_MARK, T_COLON], maxDepth: 0),
             new SeparatorGroup('comma', [T_COMMA], splitAfter: true, maxDepth: 1),
+            new SeparatorGroup('and/or', [T_BOOLEAN_AND, T_BOOLEAN_OR, T_LOGICAL_AND, T_LOGICAL_OR], maxDepth: 0),
+            new SeparatorGroup('arithmetic', [T_PLUS, T_MINUS], maxDepth: 0),
         ];
     }
 
