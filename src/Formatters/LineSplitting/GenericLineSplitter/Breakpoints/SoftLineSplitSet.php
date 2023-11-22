94505ebf03e07349d14aec742c51c1607c15e433
Patrick Morpey <patrick@morphp.nl>
2023-11-22 16:21:18 +0100

    Reformatting

--- src/Formatters/LineSplitting/GenericLineSplitter/Breakpoints/SoftLineSplitSet.php
+++ src/Formatters/LineSplitting/GenericLineSplitter/Breakpoints/SoftLineSplitSet.php
@@ -15,19 +15,26 @@ class SoftLineSplitSet implements BreakpointSet
     public function __construct()
     {
         // Sort the best breakpoints on top
-        $this->groups = [[
-            new Definition('depth 0 ternary', [T_QUESTION_MARK, T_COLON], maxDepth: 0),
-        ], [
-            new Definition('depth 0 comma', [T_COMMA], splitAfter: true, maxDepth: 0),
-            new Definition(
-                'depth 0 and/or',
-                [T_BOOLEAN_AND, T_BOOLEAN_OR, T_LOGICAL_AND, T_LOGICAL_OR],
-                maxDepth: 0
-            ),
-            new Definition('depth 0 plus/minus/concat', [T_PLUS, T_MINUS, T_CONCATENATOR, T_SLASH], maxDepth: 0),
-        ], [
-            new Definition('depth 1 comma', [T_COMMA], splitAfter: true, maxDepth: 1),
-        ]];
+        $this->groups = [
+            [
+                new Definition('depth 0 ternary', [T_QUESTION_MARK, T_COLON], maxDepth: 0),
+            ],
+            [
+                new Definition('depth 0 comma', [T_COMMA], splitAfter: true, maxDepth: 0),
+                new Definition(
+                    'depth 0 and/or',
+                    [T_BOOLEAN_AND, T_BOOLEAN_OR, T_LOGICAL_AND, T_LOGICAL_OR],
+                    maxDepth: 0
+                ),
+                new Definition('depth 0 plus/minus/concat', [T_PLUS, T_MINUS, T_CONCATENATOR], maxDepth: 0),
+            ],
+            [
+                new Definition('depth 0 slash', [T_SLASH], maxDepth: 0),
+            ],
+            [
+                new Definition('depth 1 comma', [T_COMMA], splitAfter: true, maxDepth: 1),
+            ],
+        ];
     }
 
     public function groupsOfDefinitions(): array
